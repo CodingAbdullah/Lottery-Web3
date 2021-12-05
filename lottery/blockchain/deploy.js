40a3ba25d1135000413a741735e1e55acd469fc7
Abdul <abdullahmd326@gmail.com>
2021-12-05 00:15:05 -0500

    Completed deploy.js file to Ropsten Test Network

--- lottery/blockchain/deploy.js
+++ lottery/blockchain/deploy.js
@@ -1,4 +1,29 @@
 require("dotenv").config({ path: '.env' });
-const { abi, evm } = require('../contracts/compile'); 
+const { abi, evm } = require('../blockchain/compile'); 
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
 
-// Deploying to ROPSTEN TEST NETWORK, credentials hidden
+// Deploying to ROPSTEN TEST NETWORK, credentials hidden. Add your custom in a .env file
+
+const provider = new HDWalletProvider({
+    mnemonic: {
+        phrase: process.env.MNEMONIC
+      },
+    providerOrUrl: process.env.ROPSTEN_NETWORK
+});
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    
+    const contract = await new web3.eth.Contract(abi)
+                    .deploy({ data: evm.bytecode.object })
+                    .send({ gas: 1000000, from: accounts[0] });
+
+    console.log("Contract sucessfully deployed to: " + contract.options.address); // Obtain message contract deploy succesfully to Ropsten Test Network
+    provider.engine.stop();
+};
+
+deploy(); // Run deployment
+
